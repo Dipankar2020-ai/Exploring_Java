e815cc1f40e10733ef26b6e86a45d280b62acb1e
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-08-20 20:59:57 +0530

    Create Predefinedclassasidentifier.java

---
+++ Durgasoft_Core_java/Introduction/Predefinedclassasidentifier.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+		
+		int String =20;           //Predefined String class  
+    float  Exception=30;      //predefined Exception class
+		System.out.println(String);
+		System.out.println(Exception);
+	}
+}
