7ef6eb108c2f56d9c204dd889ee0fe8725d46bbf
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-08-29 11:47:22 +0530

    Create Character to integer.java

---
+++ Durgasoft_Core_java/Flow Control/Switch statment/Character to integer.java
@@ -0,0 +1,19 @@
+//internal conversion of Character to integer
+public class Main
+{
+	public static void main(String[] args) {
+		int a=65;
+		
+		switch(a)
+		{
+		   case 66: System.out.println("10");
+		           break;
+		  case 'A':System.out.println("20");
+		          break;
+		   case 30: System.out.println("30");
+		        break;
+		  default:
+		    System.out.println("default");
+		}
+	}
+}	
