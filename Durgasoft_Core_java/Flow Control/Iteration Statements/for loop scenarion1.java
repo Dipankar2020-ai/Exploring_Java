9be091a8c4e2c73b75b7bdf5c4782a5734d97101
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-09-06 09:47:02 +0530

    Create for loop scenarion1.java

---
+++ Durgasoft_Core_java/Flow Control/Iteration Statements/for loop scenarion1.java
@@ -0,0 +1,15 @@
+//Instead of initialization it is possible to take any number of System.out.println(“ratna”) statements  and each and every statement is separated by camas(,) 
+public class Main
+{
+	public static void main(String[] args) {
+	    int i=0;
+	    for (System.out.println("Aruna");i<10;i++)
+	    {System.out.println("Rattaiah");
+	        
+	    }
+	    
+	}
+		
+	    
+	
+}
