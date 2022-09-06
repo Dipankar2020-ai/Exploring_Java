135df7755ac1a4f35c0bfcd7cd2fa284cc965e9c
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-09-06 09:50:37 +0530

    Create for loop scenarion2.java

---
+++ Durgasoft_Core_java/Flow Control/Iteration Statements/for loop scenarion2.java
@@ -0,0 +1,15 @@
+//Inside the for loop increment/decrement part is optional.
+public class Main
+{
+	public static void main(String[] args) {
+	    int i;
+	    for (i=0;i<10;)
+	    {System.out.println("Rattaiah");
+	        
+	    }
+	    
+	}
+		
+	    
+	
+}
