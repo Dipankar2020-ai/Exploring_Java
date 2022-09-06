825a630ef12c8ce64b32e1bf6e5f4c6ad1df20a6
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-09-06 09:52:57 +0530

    Create for loop scenarion3.java

---
+++ Durgasoft_Core_java/Flow Control/Iteration Statements/for loop scenarion3.java
@@ -0,0 +1,15 @@
+//Instead of increment/decrement it is possible to take the any number of SOP()  that  and each and every statement is separated by camas(,).
+public class Main
+{
+	public static void main(String[] args) {
+	    int i;
+	    for (i=0;i<10;System.out.println("aruna"))
+	    {System.out.println("Rattaiah");
+	        
+	    }
+	    
+	}
+		
+	    
+	
+}
