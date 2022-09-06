ab11746ab1081472c0eaf3a96efd79719bd906fb
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-09-06 11:40:11 +0530

    Create while loop scenario1.java

---
+++ Durgasoft_Core_java/Flow Control/Iteration Statements/while loop scenario1.java
@@ -0,0 +1,13 @@
+//compilation error unreachable statement
+public class Main
+{
+	public static void main(String[] args) {
+		int i=0;
+		while(false)
+		{
+		    System.out.println("Rattaih")	;
+		    i++;
+		}
+	    
+	}
+}
